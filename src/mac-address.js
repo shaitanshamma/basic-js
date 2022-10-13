c9f1e42821ed75178e3161e3f8e821438dcad99c
shamma <shammacode@gmail.com>
2022-10-14 00:39:26 +0300

    feat: add mac-address

--- src/mac-address.js
+++ src/mac-address.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
@@ -7,17 +7,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Your task is to check by given string inputString
  * whether it's a MAC-48 address or not.
  *
- * @param {Number} inputString
+ * @param {String} inputString
  * @return {Boolean}
  *
  * @example
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(inputString) {
+    let addressList = inputString.split('-');
+    let res = false;
+    for (let i = 0; i < addressList.length; i++) {
+        if (!isNaN(parseInt(addressList[i], 16))) {
+            res = true;
+        } else return false
+    }
+    return res;
 }
+
 module.exports = {
-  isMAC48Address
+    isMAC48Address
 };
